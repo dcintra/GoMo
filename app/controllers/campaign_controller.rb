88099452d07a76f6280340f9d1964b188208779b
Daniel Cintra <daniel@lcintra.com>
2012-10-30 21:41:54 -0400

    added campaing info retrieval

--- app/controllers/campaign_controller.rb
+++ app/controllers/campaign_controller.rb
@@ -12,6 +12,23 @@ class CampaignController < ApplicationController
         @campaign_criterion = CampaignCriterion.get_criterion_list(response) #create a new object (get criterion list) with the data retrieved from API (response) 
         @campaign_criterion_count = response[:total_num_entries]
     end
+    campaign_response = call_campaign()
+    if campaign_response
+        @campaign_details = Campaign.get_campaign_info(campaign_response)
+        @campaign_details_count = response[:total_num_entries]
+    end
+  end
+
+  def check_for_hybrid
+    @campaign_criterion.each_key do |campaignId| #find the campaign_ids for the CID
+      @campaignId = campaignId
+      @campaign_criterion[campaignId.to_i].any? do |hybrid| #loop through them and check whether they are targeting both Mobile (id=30001) and Desktop (id=3000)
+        @is_hybrid = hybrid[:id] == 30000 & 30001 #if they are tell me if it's true or false
+          if @is_hybrid = true #if it's true, split out the campaign into desktop & mobile
+            "HYBRID CAMPAIN" #insert splitting campaign details here
+        end
+      end
+    end  
   end
 
   def call_campaign_criterion() #retrieve all the necessary fields from the api
@@ -23,9 +40,27 @@ class CampaignController < ApplicationController
       })
   end 
 
-  def hybrid_campaign_filter
-    hybrid_campaign = @campaigns
+  def call_campaign() #retrieve all the campaign info for specific hybrid campaign
+    campaign_srv = @adwords.service(:CampaignService, :v201209) 
+      campaign_service = campaign_srv.get({
+      :fields => ['Id', 'Name', 'Status', 'StartDate', 'EndDate', 'Settings'],
+      :predicates => [
+        {:field => 'Id', :operator => 'EQUALS', :values => ['90823468']}]
+      })
+  end 
+
+  def copy_campaign #copy campaign info and set to mobile only
   end
+
+  def update_remove_mobile #update campaign => remove mobile
+  end
+
+  def shared_budget #initiate shared budget
+  end
+   
+  
+  
+  
 
 
 end
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,17 +3,23 @@ class Campaign
   attr_reader :id
   attr_reader :name
   attr_reader :status
+  attr_reader :startdate
+  attr_reader :enddate
+  attr_reader :settings
 
   def initialize(api_campaign)
     @id = api_campaign[:id]
     @name = api_campaign[:name]
     @status = api_campaign[:status]
+    @startdate = api_campaign[:startdate]
+    @enddate = api_campaign[:enddate]
+    @settings = api_campaign[:settings]
   end
 
-  def self.get_campaigns_list(response)
+  def self.get_campaign_info(campaign_response)
     result = {}
-    if response[:entries]
-      response[:entries].each do |api_campaign|
+    if campaign_response[:entries]
+      campaign_response[:entries].each do |api_campaign|
         campaign = Campaign.new(api_campaign)
         result[campaign.id] = campaign
       end
